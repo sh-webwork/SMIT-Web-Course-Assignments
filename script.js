7e53d83e7ae3f2904d945837c77ce0aeb960cf0c
sh-webwork <160354755+sh-webwork@users.noreply.github.com>
2024-03-09 11:20:47 +0500

    Assignment 1 (JS chap 1)

---
+++ script.js
@@ -0,0 +1,5 @@
+// alert("Error! please enter a valid password");
+// alert("Welcome to JS Land... \nHappy coding");
+// alert("Welcome to JS Land");
+// var message = ("Hello... I can run JS through my web browser's console");
+// console.log(message);
