0b29a1d4852a4eacb0e33d0c6b57f6944b3b3a9b
sh-webwork <160354755+sh-webwork@users.noreply.github.com>
2024-03-09 11:24:11 +0500

    Assignment 1 (JS Chap 2)

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // alert("Error! please enter a valid password");
 // alert("Welcome to JS Land... \nHappy coding");
 // alert("Welcome to JS Land");
-// var message = ("Hello... I can run JS through my web browser's console");
+var username;
+var myName = ("Shujaat Hussain");
 // console.log(message);
+var message;
+message = "Hello World";
+alert(message);
+alert("Jhone Doe")
+alert("15 years old")
+alert("Certified Mobile Application Developer")
+var newMassage = "PIZZA \nPIZZ\nPIZ\nPI\nP";
+alert(newMassage);
+var email = "sh.official007@gamil.com";
+alert("My email address is " + email);
+var book = "A smart way to learn JavaScript"
+alert("I am trying to learn from the Book " + book);
+document.write("Yah! I can write HTML content throuhg JavaScript");
+var newMassage1 = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(newMassage1);
+document.write("<br/>" + newMassage1);
+
+
+
